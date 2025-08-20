6fe5b48b8e1dbebb8661c18e1d149a289c3f7747
Sergey Shishkin <shishkinsa1997@gmail.com>
2025-08-21 01:34:00 +0700

    feat: solve task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,38 @@
-module.exports = function check(/* str, bracketsConfig */) {
-  throw new Error('Not implemented');
+module.exports = function check(str, bracketsConfig) {
+  const openToClose = new Map(
+    bracketsConfig.map(([open, close]) => [open, close])
+  );
+  const closeToOpen = new Map(
+    bracketsConfig.map(([open, close]) => [close, open])
+  );
+  const symmetric = new Set(
+    bracketsConfig
+      .filter(([open, close]) => open === close)
+      .map(([open]) => open)
+  );
+
+  const stack = [];
+
+  const ok = str.split('').every((char) => {
+    if (symmetric.has(char)) {
+      if (stack[stack.length - 1] === char) stack.pop();
+      else stack.push(char);
+      return true;
+    }
+
+    if (openToClose.has(char)) {
+      stack.push(char);
+      return true;
+    }
+
+    if (closeToOpen.has(char)) {
+      const needOpen = closeToOpen.get(char);
+      const last = stack.pop();
+      return last === needOpen;
+    }
+
+    return false;
+  });
+
+  return ok && stack.length === 0;
 };
